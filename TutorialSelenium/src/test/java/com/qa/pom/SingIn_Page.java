4591d2b620f3d0b7fb11b0244197e35e98a775cf
Maloto7 <agustin.colque@gmail.com>
2020-01-11 13:05:08 -0400

    adding sigin POM

--- TutorialSelenium/src/test/java/com/qa/pom/SingIn_Page.java
+++ TutorialSelenium/src/test/java/com/qa/pom/SingIn_Page.java
@@ -1,10 +1,35 @@
 package com.qa.pom;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.WebDriver;
 
+import java.util.List;
+
 public class SingIn_Page extends Base {
 
+    By usernameLocator = By.name("userName");
+    By passwordLocator = By.name("password");
+    By loginButtonLocator = By.name("login");
+    By fontLocator = By.tagName("font");
+
+
     public SingIn_Page(WebDriver driver) {
+
         super(driver);
+    }
+
+    public void loginUser() throws InterruptedException {
+
+        type("test003",usernameLocator);
+        type("123456",passwordLocator);
+        click(loginButtonLocator);
+
+    }
+
+    public String registerMessage(){
+        List<WebElement> fonts = findElements(fontLocator);
+        return getText(fonts.get(7));
     }
 }
--- TutorialSelenium/src/test/java/com/qa/pom/SingIn_Test.java
+++ TutorialSelenium/src/test/java/com/qa/pom/SingIn_Test.java
@@ -1,4 +1,34 @@
 package com.qa.pom;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class SingIn_Test {
+
+    private WebDriver driver;
+    private String url;
+    SingIn_Page singInPage;
+
+    @Before
+    public void setUp(){
+        url = "http://newtours.demoaut.com/mercurywelcome.php";
+        singInPage = new SingIn_Page(driver);
+        driver = singInPage.chromeDriverConnection();
+        singInPage.visit(url);
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void testLoginrPOM() throws InterruptedException {
+        singInPage.loginUser();
+        assertEquals("Flight Details",singInPage.registerMessage());
+    }
 }
